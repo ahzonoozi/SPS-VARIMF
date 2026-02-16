49ad8905d137f7920c064c21541e90571c066da5
ahzonoozi <36269313+ahzonoozi@users.noreply.github.com>
2026-02-16 16:03:23 +0100

    Add files via upload
    Update csp_out.f90 to calculate elements

--- SPS_VARIMF/csp_out.f90
+++ SPS_VARIMF/csp_out.f90
@@ -4,29 +4,34 @@ SUBROUTINE CSP_OUT(output_name)
   IMPLICIT NONE
 
 
-  ! Inputs
+  ! ---- Inputs ----
   CHARACTER(100), INTENT(IN) :: output_name
 
-  ! Local variables
-  INTEGER :: i
+  ! ---- Local variables  ----
+  INTEGER :: i,k
   REAL(KIND(1.d0)), DIMENSION(num_time) :: lbol_ssp, mass_ssp, mrem_ssp, mdwarf_ssp, Metal_ssp
   REAL(KIND(1.d0)), DIMENSION(nspec, num_time) :: spec_ssp
-  REAL(KIND(1.d0)) :: lbol_csp, mass_csp, mrem_csp, mdwarf_csp, Metal_csp, Mgas_csp
+  REAL(KIND(1.d0)) :: lbol_csp, mass_csp, mrem_csp, mdwarf_csp
+  REAL(KIND(1.d0)) :: Metal_gas_csp, Metal_star_csp, Mgas_csp, SNIa_csp
+  REAL(KIND(1.d0)), DIMENSION(11) :: element_gas_csp
+  REAL(KIND(1.d0)), DIMENSION(11) :: element_star_csp
   REAL(KIND(1.d0)) :: age, tage, mass_frac, tsfr, dt
   REAL(KIND(1.d0)), DIMENSION(nspec) :: spec_csp
   REAL(KIND(1.d0)), DIMENSION(n_filtrs) :: mags
   REAL(KIND(1.d0)), DIMENSION(5) :: n_Ostars
   REAL(KIND(1.d0)), DIMENSION(5) :: n_Bstars
-  REAL(KIND(1.d0)), DIMENSION(num_time, 5) :: N_O, N_B
+  REAL(KIND(1.d0)), DIMENSION(num_time,5) :: N_O, N_B
+  
   INTEGER, DIMENSION(num_time+1) ::zmet_time
+  REAL, DIMENSION(num_time+1,11) :: element_time
   REAL(KIND(1.d0)), DIMENSION(n_index) :: indices
+  REAL, DIMENSION(num_time+1) :: Metal_s
+  REAL, DIMENSION(num_time+1,11) :: element_s
 
   CHARACTER(34) :: fmt
   
   ! ------------------------------------------------------------------
   
-  OPEN(25,FILE='N_OBstars.txt',STATUS='REPLACE')
-
   ! Initialize output files 
   CALL SETUP_OUTPUT(output_name, 1, num_time)
 
@@ -37,23 +42,52 @@ SUBROUTINE CSP_OUT(output_name)
   mrem_ssp = 0.
   Metal_ssp =0.
   mdwarf_ssp = 0.
+  zmet_time = 0.
+  element_time = 0.
 
 
 
  ! --- Get CSP spectra ------------------------
-
-  ! Loop over output ages.
-  DO i=1, num_time
-     age = 10.**(time_full(i)-9.)
-     WRITE(*,*)time_full(i)
-
+ 
      zmet_time(1)= zmet_ini
+     element_time(1,:) = 0.
+     element_time(1,4) = 0.25  !Y
+     element_time(1,5) = 0.75  !X
+     
 
+  !---- Loop over output ages.
+  DO i=1, num_time
+  
+     age = 10.**(time_full(i)-9.)    ! Convert log(age) to Gyr
+     WRITE(*,*)time_full(i)
+     
+     
+
+     ! Reset outputs for this time step
+     Metal_s(i) = 0.0
+     element_s(i,:) = 0.0
+      
      mass_csp=0.
+     
+     
      ! Generate composite stellar population properties  
-     CALL csp_gen(i,zmet_time,age, mass_csp, mrem_csp, mdwarf_csp, lbol_csp, spec_csp, n_Ostars, n_Bstars, Metal_csp, Mgas_csp)
+     CALL csp_gen(i,zmet_time,element_time,age,mass_csp,mrem_csp,mdwarf_csp,&
+                  lbol_csp,spec_csp,n_Ostars,n_Bstars,Metal_gas_csp,Metal_star_csp,&
+                  element_gas_csp,element_star_csp,Mgas_csp,SNIa_csp)
 
-     zmet_time(i+1)= MAX(locate(z_isoc,Metal_csp),1)
+     zmet_time(i+1)= MAX(locate(z_isoc,Metal_gas_csp),1)
+     element_time(i+1,:) = element_gas_csp(:)
+     
+     ! Truncate metallicity if age > truncation time
+     Metal_s(i) = Metal_star_csp
+     element_s(i,:) = element_star_csp(:)
+     
+     IF(age .GT. Ttrunc) THEN
+        Metal_s(i) = Metal_s(i-1)
+        element_s(i,:)= element_s(i-1,:)
+     ENDIF
+     
+     
 
      !compute indices
      IF(index_cal .EQ. 1) THEN
@@ -67,22 +101,25 @@ SUBROUTINE CSP_OUT(output_name)
      ! Calculate age and star formation history
      tage = (age - Tstart) * 10.**9
      CALL sfh_gal(0.0,tage, mass_frac, tsfr,dt)
+     
+     WRITE(50,*)  LOG10(age) + 9.0,Metal_gas_csp,(element_gas_csp(k),k=1,11),Metal_s(i),(element_s(i,k),k=1,11)
 
 
  !----------------------------------------------------------------
  !---- print and save outputs in .mag and .spec files ------------
 
     ! Format for magnitudes file
-    fmt = '(F7.4,1x,7(F8.4,1x),000(F7.3,1x))'
+    fmt = '(F7.4,1x,9(F9.5,1x),000(F7.3,1x))'
     WRITE(fmt(21:23),'(I3,1x,I4)') n_filtrs
 
 
     ! Write magnitudes to file
     WRITE(10,fmt) LOG10(age) + 9.0, LOG10(mass_csp + small_value), &
         lbol_csp, LOG10(tsfr + small_value), LOG10(mrem_csp + small_value), &
-        LOG10(mdwarf_csp + small_value), Metal_csp,&
-        (Mgas_csp + small_value), mags
-
+        LOG10(mdwarf_csp + small_value), Metal_gas_csp, Metal_s(i),&
+        (Mgas_csp + small_value),SNIa_csp, mags  
+        
+        
     ! Write spectra to file
     WRITE(20,'(4(F8.4,1x))') LOG10(age) + 9.0,&
         LOG10(mass_csp + small_value), lbol_csp, LOG10(tsfr + small_value)
@@ -101,6 +138,7 @@ SUBROUTINE CSP_OUT(output_name)
   
   ! Close OB stars file
   CLOSE(25)
+  CLOSE(50)
 
 END SUBROUTINE CSP_OUT
 
@@ -120,6 +158,12 @@ SUBROUTINE SETUP_OUTPUT(output_name, imin, imax)
   ! Local variables
   CHARACTER(34) :: fmt
   !-----------------------------------------------------!
+  IF(OB_stars==1)THEN  
+     OPEN(25,FILE='../OUTPUTS/N_OBstars.txt',STATUS='REPLACE')
+  ENDIF
+  OPEN(50,FILE='../OUTPUTS/ELEMENTS.txt',STATUS='REPLACE')
+  
+  !------------------------------------------------------!
 
 3 FORMAT('#   tau/Gyr= ',F8.3,', Tstart= 'F6.3,',Ttrunc= 'F6.3)
 
@@ -183,7 +227,7 @@ SUBROUTINE SETUP_OUTPUT(output_name, imin, imax)
   ! Write column headers for magnitudes
   WRITE(10,'("#")')
   WRITE(10,'("#   log(age) log(mass) Log(lbol) log(SFR) log(mass_rem)&
-            log(mass_wd)  Z  Mgas_csp   mags (see FILTER_LIST)")')
+            log(mass_wd)  Z_gas Z_star  Mgas_csp  num_SNIa[1e-4]  mags (see FILTER_LIST)")')
 
 
 !-----------------------------------------
@@ -255,8 +299,10 @@ SUBROUTINE SETUP_OUTPUT(output_name, imin, imax)
 
 
   ! Open indices output file
-  OPEN(30,FILE='../OUTPUTS/'//TRIM(output_name)//'.indx',&
+  IF(index_cal==1)THEN
+     OPEN(30,FILE='../OUTPUTS/'//TRIM(output_name)//'.indx',&
      STATUS='REPLACE')
+  ENDIF
      
      
      SELECT CASE (imf_type)
